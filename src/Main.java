2c9c03b1c4349764519e6e5576e06cf720fbf38c
Evgeny Pavlov <Evgenypavlov666@yandex.ru>
2024-04-08 18:24:50 +0300

    написал программу которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Введите количество строк в массиве: ");
+        int n = scanner.nextInt();
+        String[] initialArray = new String[n];
+        System.out.println("Введите строки:");
+        for (int i = 0; i < n; i++) {
+            initialArray[i] = scanner.next();
+        }
+
+
+        String[] newArray = new String[n];
+        int count = 0;
+        for (String str : initialArray) {
+            if (str.length() <= 3) {
+                newArray[count] = str;
+                count++;
+            }
+        }
+
+
+        System.out.println("Новый массив со строками длиной <= 3:");
+        for (int i = 0; i < count; i++) {
+            System.out.println(newArray[i]);
+        }
     }
 }
